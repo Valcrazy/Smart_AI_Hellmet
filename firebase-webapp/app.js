e9c72b095fdd1b7cb59d23210f1f45631ed90ff1
Valcrazy <itsVishal@outlook.in>
2025-08-22 15:08:03 +0530

    temp stash

--- firebase-webapp/app.js
+++ firebase-webapp/app.js
@@ -1,6 +1,7 @@
 // Import functions from the Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue } from "firebase/database";
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
---
+++ firebase-webapp/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+// Import functions from the Firebase SDK
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue } from "firebase/database";
